7092b2a87493ab469d16288ea87522a9dc510569
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2024-04-25 15:40:16 +0300

    Refactor Terraform configuration and UI components

--- podify/app/AddPod/page.tsx
+++ podify/app/AddPod/page.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -16,48 +17,107 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+const podcastEpisodeSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  images: z.array(
+    z.object({
+      url: z.string(),
+      height: z.number(),
+      width: z.number(),
+    })
+  ),
+  uri: z.string(),
+  release_date: z.string(),
+  duration: z.number(),
 });
 
 export default function AddPod() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof podcastEpisodeSchema>>({
+    resolver: zodResolver(podcastEpisodeSchema),
     defaultValues: {
-      username: "",
+      id: "123",
+      name: "The Joe Rogan Experience",
+      description: "The Joe Rogan Experience podcast",
+      images: [
+        {
+          url: "https://example.com/joe-rogan.jpg",
+          height: 640,
+          width: 640,
+        },
+      ],
+      uri: "https://example.com/joe-rogan",
+      release_date: "2021-09-01",
+      duration: 3600,
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof podcastEpisodeSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
+  const { control, handleSubmit } = useForm();
+
   return (
     <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <div className="max-w-md mx-auto">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
-            control={form.control}
-            name="username"
+            control={control}
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="Episode Name" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+          <FormField
+            control={control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Episode Description" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={control}
+            name="release_date"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Release Date</FormLabel>
+                <FormControl>
+                  <Input type="date" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          {/* add image */}
+          <FormField
+            control={control}
+            name="images"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <Input type="file" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </>
   );
 }
--- podify/app/page.tsx
+++ podify/app/page.tsx
@@ -1,19 +1,11 @@
-import Image from "next/image";
 import { PodList } from "./PodList";
 import { BookList } from "./BookList";
 
 export default function Home() {
   return (
     <>
-    
-    <div style={{ maxWidth: '1000px', margin: '0 auto' }}>
-        <div style={{ position: 'relative', width: '100%', height: '200px', overflow: 'hidden' }}>
-          <Image src="/hero.png" alt="Podify" layout="fill" objectFit="cover" />
-        </div>
-      </div>
         <BookList />
         <PodList />
-
     </>
   );
 }
