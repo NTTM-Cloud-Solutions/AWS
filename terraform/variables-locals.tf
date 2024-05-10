cd1845ea1e74c0c602c5aacc8a0a44b73887bfaa
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2024-05-10 17:52:31 +0300

    Refactor VPC name in Terraform variables-locals.tf

--- terraform/variables-locals.tf
+++ terraform/variables-locals.tf
@@ -4,8 +4,7 @@ locals {
       usage = "Podify"
   }
 
-  vpc_name       = "Podify-server"
-
+  vpc_name       = "Podify"
   vpc_cidr_block = "10.20.0.0/16"
   public_subnets = ["10.20.0.0/20", "10.20.16.0/20", "10.20.32.0/20"]
   ami = "ami-0004c36d1930a15e6"
