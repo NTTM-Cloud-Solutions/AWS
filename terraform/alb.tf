713cb92cc5075f89c81de206f0ab5cfa1d95fbf2
Nir Tuttnauer <nir.tuttnauer@gmail.com>
2024-06-22 21:32:09 +0300

    Update alb.tf timeout to 6 sec

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -17,8 +17,8 @@ resource "aws_lb_target_group" "app3000" {
     path                = "/"
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 15
-    timeout             = 5
+    interval            = 7
+    timeout             = 6
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
